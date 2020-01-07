46a64016353db2e5c3dc1ab4c6a5f4e4cab51ccc
a5pire <tim.lawrence@wesnet.com.au>
2020-01-07 16:12:16 +1000

    added more tests for the timer

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,16 +1,154 @@
-from parsers.timer import Timer
+import pytest
+import re
+from parsers.timer import Timer, TimerError
+
+TIME_PREFIX = "Wasted time:"
+TIME_MESSAGE = f"{TIME_PREFIX} {{:.4f}} seconds"
+RE_TIME_MESSAGE = re.compile(TIME_PREFIX + r" 0\.\d{4} seconds")
+
+
+@Timer(text=TIME_MESSAGE)
+def timewaster(num):
+    """Just waste a little bit of time"""
+    sum(n ** 2 for n in range(num))
+
+
+@Timer(name="accumulator", text=TIME_MESSAGE)
+def accumulated_timewaste(num):
+    """Just waste a little bit of time"""
+    sum(n ** 2 for n in range(num))
+
+
+class CustomLogger:
+    """Simple class used to test custom logging capabilities in Timer"""
+
+    def __init__(self):
+        self.messages = ""
+
+    def __call__(self, message):
+        self.messages += message
 
 
 class TestTimer:
 
-    def test_timer(self):  # tests for when start time has a value (not none)
-        timer = Timer('Pairings')
+    @staticmethod
+    def test_timer_as_decorator(capsys):
+        """Test that decorated function prints timing information"""
+        timewaster(1000)
+        stdout, stderr = capsys.readouterr()
+        assert RE_TIME_MESSAGE.match(stdout)
+        assert stdout.count("\n") == 1
+        assert stderr == ""
 
-        assert timer == Timer(name='Pairings', text='Elapsed time: {:0.4f} seconds')
+    @staticmethod
+    def test_timer_as_context_manager(capsys):
+        """Test that timed context prints timing information"""
+        with Timer(text=TIME_MESSAGE):
+            sum(n ** 2 for n in range(1000))
+        stdout, stderr = capsys.readouterr()
+        assert RE_TIME_MESSAGE.match(stdout)
+        assert stdout.count("\n") == 1
+        assert stderr == ""
 
-    def test_start_time_is_none(self):
-        timer = Timer()
-        nil_time = None
-        assert nil_time == timer.start()
+    @staticmethod
+    def test_explicit_timer(capsys):
+        """Test that timed section prints timing information"""
+        t = Timer(text=TIME_MESSAGE)
+        t.start()
+        sum(n ** 2 for n in range(1000))
+        t.stop()
+        stdout, stderr = capsys.readouterr()
+        assert RE_TIME_MESSAGE.match(stdout)
+        assert stdout.count("\n") == 1
+        assert stderr == ""
 
-    # def test_stop_time_when_start_time_is_none(self):
+    @staticmethod
+    def test_error_if_timer_not_running():
+        """Test that timer raises error if it is stopped before started"""
+        t = Timer(text=TIME_MESSAGE)
+        with pytest.raises(TimerError):
+            t.stop()
+
+    @staticmethod
+    def test_access_timer_object_in_context(capsys):
+        """Test that we can access the timer object inside a context"""
+        with Timer(text=TIME_MESSAGE) as t:
+            assert isinstance(t, Timer)
+            assert t.text.startswith(TIME_PREFIX)
+        _, _ = capsys.readouterr()  # Do not print log message to standard out
+
+    @staticmethod
+    def test_custom_logger():
+        """Test that we can use a custom logger"""
+        logger = CustomLogger()
+        with Timer(text=TIME_MESSAGE, logger=logger):
+            sum(n ** 2 for n in range(1000))
+        assert RE_TIME_MESSAGE.match(logger.messages)
+
+    @staticmethod
+    def test_timer_without_text(capsys):
+        """Test that timer with logger=None does not print anything"""
+        with Timer(logger=None):
+            sum(n ** 2 for n in range(1000))
+
+        stdout, stderr = capsys.readouterr()
+        assert stdout == ""
+        assert stderr == ""
+
+    @staticmethod
+    def test_accumulated_decorator(capsys):
+        """Test that decorated timer can accumulate"""
+        accumulated_timewaste(1000)
+        accumulated_timewaste(1000)
+
+        stdout, stderr = capsys.readouterr()
+        lines = stdout.strip().split("\n")
+        assert len(lines) == 2
+        assert RE_TIME_MESSAGE.match(lines[0])
+        assert RE_TIME_MESSAGE.match(lines[1])
+        assert stderr == ""
+
+    @staticmethod
+    def test_accumulated_context_manager(capsys):
+        """Test that context manager timer can accumulate"""
+        t = Timer(name="accumulator", text=TIME_MESSAGE)
+        with t:
+            sum(n ** 2 for n in range(1000))
+        with t:
+            sum(n ** 2 for n in range(1000))
+
+        stdout, stderr = capsys.readouterr()
+        lines = stdout.strip().split("\n")
+        assert len(lines) == 2
+        assert RE_TIME_MESSAGE.match(lines[0])
+        assert RE_TIME_MESSAGE.match(lines[1])
+        assert stderr == ""
+
+    @staticmethod
+    def test_accumulated_explicit_timer(capsys):
+        """Test that explicit timer can accumulate"""
+        t = Timer(name="accumulated_explicit_timer", text=TIME_MESSAGE)
+        total = 0
+        t.start()
+        sum(n ** 2 for n in range(1000))
+        total += t.stop()
+        t.start()
+        sum(n ** 2 for n in range(1000))
+        total += t.stop()
+
+        stdout, stderr = capsys.readouterr()
+        lines = stdout.strip().split("\n")
+        assert len(lines) == 2
+        assert RE_TIME_MESSAGE.match(lines[0])
+        assert RE_TIME_MESSAGE.match(lines[1])
+        assert stderr == ""
+        assert total == Timer.timers["accumulated_explicit_timer"]
+
+    @staticmethod
+    def test_error_if_restarting_running_timer():
+        """Test that restarting a running timer raises an error"""
+        t = Timer(text=TIME_MESSAGE)
+        t.start()
+        with pytest.raises(TimerError):
+            t.start()
+
