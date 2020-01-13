37554244b1b59c045c2ab7fc241b850e5329cbb0
a5pire <tim.lawrence@wesnet.com.au>
2020-01-13 18:25:02 +1000

    added comments to code

--- mongo.py
+++ mongo.py
@@ -7,22 +7,22 @@ from dotenv import load_dotenv
 
 def database_insertion(json_file):
 
-    load_dotenv()
+    load_dotenv()   # initialises the environment variables from .env file
 
     with open(json_file) as f:
-        database_input_file = json.load(f)
+        database_input_file = json.load(f)  # loads json data into database input variable
 
         cluster = MongoClient(f"mongodb+srv://a5pire:{os.getenv('mongo')}@pairings-d8pll.gcp.mongodb.net/"
-                              "test?retryWrites=true&w=majority")
-        db = cluster['pairings']
+                              "test?retryWrites=true&w=majority")   # connects to the mongodb server with user/pass
+        db = cluster['pairings']    # selects pairings database from server
         print()
-        rp = db[input('Please enter database collection name (rpXX_20XX): ')]
+        rp = db[input('Please enter database collection name (rpXX_20XX): ')]   # creates a new collection name variable
 
         print(f'Inserting documents....')
 
-        start = time.perf_counter()
-        rp.insert_many(database_input_file)
-        end = time.perf_counter()
+        start = time.perf_counter()     # starts timing before inserting the documents to the database
+        rp.insert_many(database_input_file)     # inserts new collection into database
+        end = time.perf_counter()   # finishes timing on completion of the document insertion
 
         print(f'Completed in {round((end - start), 4)} seconds')
         print()
