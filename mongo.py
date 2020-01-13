2356e5c3015c548fb6eba430a9b54a6cd1101254
a5pire <tim.lawrence@wesnet.com.au>
2020-01-13 11:01:00 +1000

    added mongodb insertion function

--- mongo.py
+++ mongo.py
@@ -1,3 +1,23 @@
+import json
+import time
+from pymongo import MongoClient
 
-def mongo(data_file):
-    pass
+
+def database_insertion(json_file):
+
+    with open(json_file) as f:
+        database_input_file = json.load(f)
+
+        cluster = MongoClient("mongodb+srv://a5pire:PW7XxSS23mg3tSLX@pairings-d8pll.gcp.mongodb.net/"
+                              "test?retryWrites=true&w=majority")
+        db = cluster['pairings']
+        print()
+        rp = db[input('Please enter database collection name (rpXX_20XX): ')]
+
+        print(f'Inserting documents....')
+
+        start = time.perf_counter()
+        rp.insert_many(database_input_file)
+        end = time.perf_counter()
+
+        print(f'Completed in {round((end - start), 4)} seconds')
